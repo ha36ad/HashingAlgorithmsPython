0485b775862eed22f361496f5cd580c1ce5638e9
Hammad Khan <59400114+hailaider@users.noreply.github.com>
2020-06-02 02:36:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uuid
 
 #Global constants
 X = 0 
-
+SECRET=secrets.tokenhex(16)
 #message to encode
 message=input("Input a message: ")
 
@@ -17,28 +17,42 @@ while X == 0:
 #pick between 256 and 512 bytes
         hash_type = input ("Input byte size (256 or 512): ")
         
-#SHA2
+        #SHA2
         if hashh == "SHA-2" and hash_type == "256":
-                print("Your hash in hexadecimal is: ", hashlib.sha256(encoded_message).hexdigest())
+                #Combine the hash and secret
+                combination = hashlib.sha256(encoded_message).hexdigest()+ SECRET
+                print ("Your hash in hexadecimal is:", combination)                
                 break
         elif hashh == "SHA-2" and hash_type == "512":
-                print("Your hash in hexadecimal is: ", hashlib.sha512(encoded_message).hexdigest())
+                #Combine the hash and secret
+                combination = hashlib.sha512(encoded_message).hexdigest()+ SECRET
+                print ("Your hash in hexadecimal is:", combination)                
                 break
+        
         #SHA3
         elif hashh == "SHA-3" and hash_type == "256":
-                print ("Your hash in hexadecimal is ", hashlib.sha3_256(encoded_message).hexdigest())
+                #Combine the hash and secret
+                combination = hashlib.sha3_256(encoded_message).hexdigest()+ SECRET
+                print ("Your hash in hexadecimal is:", combination)
                 break
         elif hashh == "SHA-3" and hash_type == "512":
-                print ("Your hash in hexadecimal is ", hashlib.sha3_512(encoded_message).hexdigest())
+                #Combine the hash and secret
+                combination = hashlib.sha3_512(encoded_message).hexdigest()+ SECRET
+                print ("Your hash in hexadecimal is:", combination)
                 break
             
         #BLAKE2b(256) or BLAKE2s(512)
         elif hashh == "BLAKE2" and hash_type == "256":
-                print ("Your hash in hexadecimal is ", hashlib.blake2s(encoded_message).hexdigest())
+                #Combine the hash and secret
+                combination = hashlib.blake2s(encoded_message).hexdigest()+ SECRET
+                print ("Your hash in hexadecimal is:", combination)
                 break
         elif hashh == "BLAKE2" and hash_type == "512":
-                print ("Your hash in hexadecimal is ", hashlib.blake2b(encoded_message).hexdigest())
+                #Combine the hash and secret
+                combination = hashlib.blake2b(encoded_message).hexdigest()+ SECRET
+                print ("Your hash in hexadecimal is:", combination)
                 break
+
         
 
         
