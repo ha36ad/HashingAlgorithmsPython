91e8ba0dc47c82eeaf954f8551a5573c57377a36
Hammad Khan <59400114+hailaider@users.noreply.github.com>
2020-06-02 02:05:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,27 +17,27 @@ while X == 0:
 #pick between 256 and 512 bytes
         hash_type = input ("Input byte size (256 or 512): ")
         
-        #converts from bytes to hexadecimal        
-        #SHA2
+#SHA2
         if hashh == "SHA-2" and hash_type == "256":
-                print("Your hash is: ", hashlib.sha256(encoded_message).hexdigest())
+                print("Your hash in hexadecimal is: ", hashlib.sha256(encoded_message).hexdigest())
                 break
         elif hashh == "SHA-2" and hash_type == "512":
-                print("Your hash is: ", hashlib.sha512(encoded_message).hexdigest())
+                print("Your hash in hexadecimal is: ", hashlib.sha512(encoded_message).hexdigest())
                 break
         #SHA3
         elif hashh == "SHA-3" and hash_type == "256":
-                print ("Your hash is ", hashlib.sha3_256(encoded_message).hexdigest())
+                print ("Your hash in hexadecimal is ", hashlib.sha3_256(encoded_message).hexdigest())
                 break
         elif hashh == "SHA-3" and hash_type == "512":
-                print ("Your hash is ", hashlib.sha3_512(encoded_message).hexdigest())
+                print ("Your hash in hexadecimal is ", hashlib.sha3_512(encoded_message).hexdigest())
                 break
+            
         #BLAKE2b(256) or BLAKE2s(512)
         elif hashh == "BLAKE2" and hash_type == "256":
-                print ("Your hash is ", hashlib.blake2s(encoded_message).hexdigest())
+                print ("Your hash in hexadecimal is ", hashlib.blake2s(encoded_message).hexdigest())
                 break
         elif hashh == "BLAKE2" and hash_type == "512":
-                print ("Your hash is ", hashlib.blake2b(encoded_message).hexdigest())
+                print ("Your hash in hexadecimal is ", hashlib.blake2b(encoded_message).hexdigest())
                 break
         
 
