924769fb49a25168fd0209eb102088f577105f80
Hammad Khan <59400114+hailaider@users.noreply.github.com>
2020-06-02 01:28:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 import hashlib
 import secrets 
+import uuid
+
+#Global constants
+X = 0 
+
+#message to encode
+message=input("Input a message: ")
+
+#encode the message
+encoded_message = message.encode()
+
+#pick your type of hash algorithm
+while X == 0:
+        hashh = input("Input hash type (SHA-2,SHA-3,BLAKE2): ")
+        if hashh == "SHA-2":
+                break
+        elif hashh == "SHA-3":
+                break
+        elif hashh == "BLAKE2":
+                break
+        
+        
