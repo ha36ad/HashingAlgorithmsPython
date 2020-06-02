7193736d8f25c3e016e5b8fad5f789400888c5a6
Hammad Khan <59400114+hailaider@users.noreply.github.com>
2020-06-02 01:46:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,16 @@ encoded_message = message.encode()
 #pick your type of hash algorithm
 while X == 0:
         hashh = input("Input hash type (SHA-2,SHA-3,BLAKE2): ")
+        
+        #converts from string to bytes, then to hexadecimal
         if hashh == "SHA-2":
+                print("Your hash is: ", hashlib.sha256(encoded_message).hexdigest())
                 break
         elif hashh == "SHA-3":
+                print ("Your hash is ", hashlib.sha3_512(encoded_message).hexdigest())
                 break
         elif hashh == "BLAKE2":
+                print ("Your hash is ", hashlib.blake2s(encoded_message).hexdigest())
                 break
         
         
