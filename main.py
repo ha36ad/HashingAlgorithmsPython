cdcca258367b1fb4eae6946a797eed44c240cc44
Hammad Khan <59400114+hailaider@users.noreply.github.com>
2020-06-02 01:57:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,16 +14,32 @@ encoded_message = message.encode()
 #pick your type of hash algorithm
 while X == 0:
         hashh = input("Input hash type (SHA-2,SHA-3,BLAKE2): ")
+#pick between 256 and 512 bytes
+        hash_type = input ("Input byte size (256 or 512): ")
         
-        #converts from string to bytes, then to hexadecimal
-        if hashh == "SHA-2":
+        #converts from bytes to hexadecimal        
+        #SHA2
+        if hashh == "SHA-2" and hash_type == "256":
                 print("Your hash is: ", hashlib.sha256(encoded_message).hexdigest())
                 break
-        elif hashh == "SHA-3":
+        elif hashh == "SHA-2" and hash_type == "512":
+                print("Your hash is: ", hashlib.sha512(encoded_message).hexdigest())
+                break
+        #SHA3
+        elif hashh == "SHA-3" and hash_type == "256":
+                print ("Your hash is ", hashlib.sha3_256(encoded_message).hexdigest())
+                break
+        elif hashh == "SHA-3" and hash_type == "512":
                 print ("Your hash is ", hashlib.sha3_512(encoded_message).hexdigest())
                 break
-        elif hashh == "BLAKE2":
+        #BLAKE2b(256) or BLAKE2s(512)
+        elif hashh == "BLAKE2" and hash_type == "256":
                 print ("Your hash is ", hashlib.blake2s(encoded_message).hexdigest())
                 break
+        elif hashh == "BLAKE2" and hash_type == "512":
+                print ("Your hash is ", hashlib.blake2b(encoded_message).hexdigest())
+                break
+        
+
         
         
